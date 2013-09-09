30d2781fd945eccffd760de9d12ffa30ef6a7987
Lebohang Danster <lebohang@Lebohangs-MacBook-Pro.local>
2013-09-09 14:27:05 +0200

    mathabo/Lebo calculating measurement using enums

--- src/Measurement.java
+++ src/Measurement.java
@@ -1,2 +1,17 @@
-public class Measurement {
+public final class Measurement {
+    private final double value;
+    private final Unit unit;
+
+    public Measurement(double value, Unit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+         Measurement other = (Measurement)obj;
+
+        return this.unit.returnInch(this.value) == other.unit.returnInch(other.value);
+
+    }
 }
--- src/Unit.java
+++ src/Unit.java
@@ -1,2 +1,14 @@
-public class Unit {
-}
+public enum Unit {
+    Inch(1),
+    Foot(0.83333);
+
+    double unitValue;
+
+    Unit(double unitValue) {
+         this.unitValue = unitValue;
+    }
+
+    double returnInch(double value) {
+       return this.unitValue * value;
+    }
+ }
